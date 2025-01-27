6ed2aa2a095bbe429a52df1aebbb30870bc6f0ad
Lucas da Silva Pimentel <pimentellucas753@gmail.com>
2025-01-27 10:40:42 -0300

    primeiro commit

---
+++ src/main/java/com/generation/farmatel/FarmatelApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmatelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmatelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmatel/FarmatelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmatel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmatelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
