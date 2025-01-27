6406460cb8ac4600e5c94883ca3427a6b8aae840
Lucas da Silva Pimentel <pimentellucas753@gmail.com>
2025-01-27 14:08:21 -0300

    terceira etapa

--- src/main/java/com/generation/farmatel/model/Categoria.java
+++ src/main/java/com/generation/farmatel/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.farmatel.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -24,6 +31,10 @@ public class Categoria {
 	@Size (min = 4, max = 100, message = "A descricao deve possuir de 4 a 100 caracteres")
 	private String decricao;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
 	public Long getId() {
 		return id;
 	}
@@ -47,5 +58,14 @@ public class Categoria {
 	public void setDecricao(String decricao) {
 		this.decricao = decricao;
 	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
 	
 }
---
+++ src/main/java/com/generation/farmatel/model/Produto.java
@@ -0,0 +1,92 @@
+package com.generation.farmatel.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Esse campo é obrigatório")
+	@Size(min = 4, max = 100, message = "O nome deve conter no de 4 a 100 caracteres")
+	private String nome;
+	
+	private double preco;
+	
+	private double estoque;
+	
+	@NotBlank(message = "Esse campo é obrigatório")
+	@Size(min = 2, max = 100, message = "O fabricante deve conter de 2 a 100 caracteres")
+	private String fabricante;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(double estoque) {
+		this.estoque = estoque;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
